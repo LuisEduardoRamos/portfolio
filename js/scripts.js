fc04fa64fc50d89de697bb5ffd713756c95776b2
Luis\Ramos <luiseduardoramosg@gmail.com>
2024-04-02 04:42:52 -0700

    Update profile pic and form refact

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,28 @@
-const submitButtonId = document.getElementById('submitButton');
-const errorMessage = document.getElementById('submitErrorMessage');
-
-const getMessageValue = () => {
-  const textArea = document.getElementById('message');
-  return textArea.value;
-};
+const submitButtonId = document.getElementById("submitButton");
+const errorMessage = document.getElementById("submitErrorMessage");
 
 const showError = () => {
-  errorMessage.classList.remove('d-none');
+  errorMessage.classList.remove("d-none");
 };
 
 const clearError = () => {
-  errorMessage.classList.add('d-none');
+  errorMessage.classList.add("d-none");
 };
 
-submitButtonId.addEventListener('click', () => {
-  const message = getMessageValue();
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const phone = document.getElementById('phone').value;
+submitButtonId.addEventListener("click", async () => {
+  submitButtonId.disabled = true;
+  const message = document.getElementById("message").value;
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
   let error = false;
   [(message, name, email, phone)].forEach((item) => {
     if (item.length < 1) error = true;
   });
   if (error) {
     showError();
+    submitButtonId.disabled = false;
   } else {
     clearError();
-    //TODO REQUEST TO LR API
   }
 });
